9f917ddbd834e82168e32833e31dafecbed20bb9
TrinhChung <chungtrinh2k2@gmail.com>
2023-07-18 16:23:18 +0700

    Show pitch information

--- pitch/urls.py
+++ pitch/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
+from project1 import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("list/", views.PitchListView.as_view(), name="pitch-list"),
+    path("<int:pk>", views.PitchDetailView.as_view(), name="pitch-detail"),
 ]
--- project1/urls.py
+++ project1/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", RedirectView.as_view(url="login/")),
     path("i18n/", include("django.conf.urls.i18n")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # accounts/ login/ [name='login']
 # accounts/ logout/ [name='logout']
