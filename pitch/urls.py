2a432e8016c3004364834920f3e769bdf2be8a12
20522182 <20522182@gm.uit.edu.vn>
2023-08-09 15:42:28 +0700

    Let admin seed data from Excel
    Let admin seed data from Excel

--- pitch/urls.py
+++ pitch/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("ordered/", views.MyOrderedView.as_view(), name="my-ordered"),
     path("ordered-detail/<int:pk>", views.order_cancel, name="order-detail"),
     path('search/', views.search_view, name='search'),
+    path('upload-pitch-data/', views.upload_pitch_data, name='upload_pitch_data'),
 ]
